e5eb2094d7f0a28f1858b36b925b9ae9e89d0f7a
rahmafeidi <rahmafeidi12@gmail.com>
2023-11-24 15:22:55 +0100

    my project

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-/*
 
-GEREKLİ PAKETLER YÜKLENİYOR...
-
-*/
 var http = require('http');
 var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
-app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
+app.set('port', process.env.PORT || 3005); 
+app.set('views', __dirname + '/app/server/views'); 
+app.set('view engine', 'ejs'); 
+app.use(express.static(__dirname + '/app/public')); 
 
-require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
+require('./app/routes')(app);
 
-/*
-
-HTTP SERVER OLUŞTURULDU
-
-*/
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+	console.log('system ' + app.get('port') + ' listen  ');
 });
